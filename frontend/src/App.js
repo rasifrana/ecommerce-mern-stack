3c7b42396f9928604711c7501650e3093f8704a6
Asif Rana <muhammadasif@MUHAMMADS-MBP.station>
2023-07-22 18:28:27 +0100

    Add Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+
 
 const App = () => {
   return (
-    <h1>Welcome to eCommerce website</h1>
+    <>
+     <Header />
+     <main className='py-3'>
+        <Container>
+        <h1>Welcome to eShop</h1>
+        </Container>
+     </main>
+    </>
   )
 }
 
