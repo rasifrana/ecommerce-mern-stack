7806782536301e467785b003c86d2d7b21cd003a
John DoeAsif <rasifrana@hotmail.com>
2023-07-25 15:25:37 +0100

    updated Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <h1>Welcome to eShop</h1>
         </Container>
      </main>
+     <Footer />
     </>
   )
 }
