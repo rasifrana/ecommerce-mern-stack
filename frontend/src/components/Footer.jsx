206e0d59f7afb3c2e990936320b9893af35e0fa9
Asif Rana <rasifrana@hotmail.com>
2023-07-25 15:27:56 +0100

    update footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className='text-center py-3'>
-            <p>eShop &copy; {currentYear}</p>
+            <p>eShop &copy; {currentYear}. All rights reserved</p>
           </Col>
         </Row>
       </Container>
